ec3691f53fcd2d7d13067b47e760b16d7c1f860d
Kostiantyn <cashalot80@ukr.net>
2022-06-09 11:49:55 +0100

    end task#1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,72 @@
+
+// Описан в документации
+import SimpleLightbox from "simplelightbox";
+// Дополнительный импорт стилей
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+function createGalleryImg(galleryItems) {
+    const galleryItemsMarkUp = galleryItems
+    .map(({ preview, original, description }) => {
+        return `
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+    </div>
+    `;
+        })
+        .join("");
+    return galleryItemsMarkUp;
+    }
+gallery.innerHTML = createGalleryImg(galleryItems);
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, captionsData:`alt`});
+
 console.log(galleryItems);
+
+
+
+// const galleryCont = document.querySelector("div.gallery");
+// const galleryMarkUp = createGalleryImg(galleryItems);
+
+// galleryCont.insertAdjacentHTML("beforeend", galleryMarkUp);
+
+// function createGalleryImg(galleryItems) {
+//     return galleryItems
+//         .map(({ preview, original, description }) => {
+//         return `
+//         <div class="gallery__item">
+//     <a class="gallery__link" href="${original}">
+//         <img
+//         class="gallery__image"
+//         src="${preview}"
+//         data-source="${original}"
+//         alt="${description}"
+//         />
+//     </a>
+//     </div>
+//     `;
+//         })
+//         .join("");
+//     }
+//     galleryCont.addEventListener("click", openImg)
+    
+//     function openImg(event) {
+//         event.preventDefault();
+//         if (!event.target.classList.contains("gallery__image")) {
+//             return;
+//         }
+//         const instance = basicLightbox.create(
+//             `<img src="${event.target.dataset.source}" alt="${event.target.dataset.description}">`
+//         );
+//             instance.show();
+//     }
