ee70274dc0df28e8db39e733312fc5658fceaa8d
Kostiantyn <cashalot80@ukr.net>
2022-06-09 16:32:03 +0100

    end task#2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player  from "@vimeo/player";
 
+    const iframe = document.querySelector('iframe');
+    const player = new Vimeo.Player(iframe);
+    const currentTimeVideo = "videoplayer-current-time";
+const throttle = require(`lodash.throttle`);
+const onPlay = function (data) {
+    localStorage.setItem(currentTimeVideo, data.seconds);
+    console.log(`${currentTimeVideo} : ${data.seconds}`)
+    // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem(currentTimeVideo)) {
+    player.setCurrentTime(localStorage.getItem(currentTimeVideo));
+   }
