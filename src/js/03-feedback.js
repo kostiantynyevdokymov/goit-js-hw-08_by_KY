3a7fe27f1abb177cc3741128a7d9190dc5472c4d
Kostiantyn <cashalot80@ukr.net>
2022-06-10 13:50:58 +0100

    update task#3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,41 @@
+const throttle = require('lodash.throttle');
 
-const inputForm = document.querySelector(`.feedback-form`);
-const keyStorage = "feedback-form-state";
-const throttle = require(`lodash.throttle`);
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('[name = "email"]');
+const messageInput = document.querySelector('[name = "message"]');
 
+const KEY = "feedback-form-state";
 
+form.addEventListener(`emailInput`, throttle(inputHandler, 500));
+form.addEventListener(`messageInput`, submitHandler);
+
+firstPageLoading();
+
+function firstPageLoading() {
+    const parseValue = JSON.parse(localStorage.getItem(KEY));
+    if (parseValue) {
+        emailInput.value = parseValue.email;
+        messageInput.value = parseValue.message;
+    }
+    else {
+        emailInput.value = '';
+        messageInput.value = '';
+    }
+}
+
+function inputHandler() {
+    const valueObj = {
+        email: emailInput.value,
+        message: messageInput.value,
+    };
+          console.log(`email: ${valueObj.email}`);
+    console.log(`message: ${valueObj.message}`);
+    localStorage.setItem(KEY, JSON.stringify(valueObj));
+}
+
+function submitHandler(event) {
+    event.preventDefault();
+    form.reset();
+    console.log(localStorage.getItem(KEY));
+    localStorage.removeItem(KEY);
+}
